a96cbb6351c24e30093252dd1e73cdf08174a254
Gustavo Santos <gugahh.br@gmail.com>
2021-09-20 18:59:06 -0400

    Atualizando PodmanLogin

--- podmanlogin.sh
+++ podmanlogin.sh
@@ -1,1 +1,1 @@
-podman login -u mprj_gustavo_santos+gugahhrobot -p CVQX8LTUYG2ZDOFDYZHAMD0NQ6OU0ILNQ2PCWCHA3LL5P05VSQBV101SR5X10FQD quay.io
+podman login -u="gugahhbr" -p="jqyJf7FMTUi4bxc5XXH6aOnQUpQR4XT2hSwX9GNwwdMlvaIoczVPulE10GgnQIMBo5QdnyqKtoVr/kYobTVe9vfBiqKBJq8gf/+vwDsj/lA=" quay.io
