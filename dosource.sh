8c4d09f576447ae735c2cd451861f8caa9bea763
Gustavo Santos <gugahh.br@gmail.com>
2021-09-21 15:24:12 -0400

    Shell script p variaveis de ambiente OCP4

---
+++ dosource.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /usr/local/etc/ocp4.config
+echo .
+echo ------ verificando a configuração ------
+cat /usr/local/etc/ocp4.config
+echo ----------------------------------------
+echo .
