4b206d02c90d594e41b0738d454886f5da619b92
Gustavo.Santos <gugahh.br@gmail.com>
2021-10-12 17:49:00 -0300

    Tentando novamente quay.io

--- podmanlogin.sh
+++ podmanlogin.sh
@@ -1,1 +1,1 @@
-podman login -u="gugahhbr" -p="jqyJf7FMTUi4bxc5XXH6aOnQUpQR4XT2hSwX9GNwwdMlvaIoczVPulE10GgnQIMBo5QdnyqKtoVr/kYobTVe9vfBiqKBJq8gf/+vwDsj/lA=" quay.io
+podman login -u="gugahhbr" -p="ob1knobB!" quay.io
